34752aa2d3d4f4f87e3d15af96a76d6a84f4e404
Michal Batko <batko@fi.muni.cz>
2009-12-01 17:29:08 +0000

    * Fixed RegionShape hashcode and equals bug and made it more memory efficient (and compatible with JNI extractor access) * WARNING: this change is not serialization compatible (i.e. all structures that use ObjectRegionShape must be rebuilded)!!

--- messif/objects/impl/ObjectRegionShape.java
+++ messif/objects/impl/ObjectRegionShape.java
@@ -23,34 +23,35 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
     /** Class id for serialization. */
     private static final long serialVersionUID = 26501L;
 
-    /****************** Coefficients ******************/
+    //****************** Coefficients ******************//
 
-	protected static double [] quantTable = {0.000000000, 0.003585473, 0.007418411, 0.011535520, 0.015982337, 0.020816302, 0.026111312,
+    /** Internal quantization table */
+    protected static double [] quantTable = {0.000000000, 0.003585473, 0.007418411, 0.011535520, 0.015982337, 0.020816302, 0.026111312,
             0.031964674, 0.038508176, 0.045926586, 0.054490513, 0.064619488, 0.077016351, 0.092998687, 0.115524524, 0.154032694, 1.000000000};
-	protected static double [] iQuantTable = {0.001763817, 0.005468893, 0.009438835, 0.013714449, 0.018346760, 0.023400748, 0.028960940,
+    /** Internal inverse quantization table */
+    protected static double [] iQuantTable = {0.001763817, 0.005468893, 0.009438835, 0.013714449, 0.018346760, 0.023400748, 0.028960940,
             0.035140141, 0.042093649, 0.050043696, 0.059324478, 0.070472849, 0.084434761, 0.103127662, 0.131506859, 0.192540857};
 
+    /** Number of angular items */
     protected static int ART_ANGULAR = 12;
+    /** Number of radial items */
     protected static int ART_RADIAL = 3;
     
 
     /****************** Attributes ******************/
 
     /** An array of RegionShape coefficients - these are indexes to the static tables above */
-	private byte [] [] m_ArtDE = new byte [ART_ANGULAR][ART_RADIAL];
+    private final byte[] artDE;
 
 
     /****************** Constructors ******************/
 
     /**
-     * Initialize all coefficients to "0".
+     * Create new instance of ObjectRegionShape.
+     * The descriptor is initialized to zeros.
      */
     public ObjectRegionShape() {
-        for (int i = 0; i < ART_ANGULAR; i++) {
-            for (int j = 0; j < ART_RADIAL; j++) {
-                m_ArtDE[i][j] = 0;
-            }
-        }
+        artDE = new byte [ART_ANGULAR*ART_RADIAL];
     }
 
     /**
@@ -70,11 +71,9 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
 
         String[] numbers = line.trim().split("[, ]+");
 
-        for (int i = 0; i < ART_ANGULAR; i++) {
-            for (int j = 0; j < ART_RADIAL; j++) {
-                this.m_ArtDE[i][j] = Byte.parseByte(numbers[(i * ART_RADIAL) + j]);
-            }
-        }
+        this.artDE = new byte [ART_ANGULAR*ART_RADIAL];
+        for (int i = 0; i < this.artDE.length; i++)
+            this.artDE[i] = Byte.parseByte(numbers[i]);
     }
 
     /**
@@ -84,66 +83,17 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
      */
     @Override
     protected void writeData(OutputStream stream) throws IOException {
-        for (int i = 0; i < ART_ANGULAR; i++) {
-            for (int j = 0; j < ART_RADIAL; j++) {
-                stream.write(String.valueOf(m_ArtDE[i][j]).getBytes());
-                if (i + 1 < ART_ANGULAR || j + 1 < ART_RADIAL) {
-                    stream.write(',');
-                    stream.write(' ');
-                }
+        for (int i = 0; i < this.artDE.length; i++) {
+            if (i != 0) {
+                stream.write(',');
+                stream.write(' ');
             }
+            stream.write(String.valueOf(artDE[i]).getBytes());
         }
         stream.write('\n');
     }
 
     // **********************************    Advanced  getters and setters    ********************************** //
-
-    /**
-     * Classical setter to the m_ArtDE table
-     * @param p index1
-     * @param r index2
-     * @param value value to store (byte)
-     * @return true if set, false if the indexes are out of dimensions
-     */
-    boolean setElement(int p, int r, byte value) {
-        if (p <0 || p >= ART_ANGULAR || r < 0 || r >= ART_RADIAL) {
-            return false;
-        }
-        m_ArtDE[p][r] = value;
-        return true;
-    }
-
-    /**
-     * Set the elelement according to quantization in the static fields.
-     * @param p index1
-     * @param r index2
-     * @param value double value to find in the quantization fields
-     * @return true if set, false if the indexes are out of dimensions
-     */
-    boolean setElement(int p, int r, double value) {
-        if (p < 0 || p >= ART_ANGULAR || r < 0 || r >= ART_RADIAL ||
-                value > 1.0 || value < 0.0) {
-            return false;
-        }
-
-        // Quantization
-        int high = 17;
-        int low = 0;
-        int middle;
-
-        while (high - low > 1) {
-            middle = (high + low) / 2;
-
-            if (quantTable[middle] < value) {
-                low = middle;
-            } else {
-                high = middle;
-            }
-        }
-
-        m_ArtDE[p][r] = (byte) low;
-        return true;
-    }
 
     /**
      * Get element from the array - index to the quantization arrays.
@@ -152,11 +102,7 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
      * @return index to the quantization arrays
      */
     public byte getElement(int p, int r) {
-        if (p < 0 || p >= ART_ANGULAR || r < 0 || r >= ART_RADIAL) {
-            return -1;
-        }
-
-        return m_ArtDE[p][r];		// Always positive value
+        return artDE[(p * ART_RADIAL) + r];
     }
 
     /**
@@ -166,12 +112,8 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
      * @return value from the quantization array
      */
     public double getRealValue(int p, int r) {
-        if (p < 0 || p >= ART_ANGULAR || r < 0 || r >= ART_RADIAL) {
-            return -1;
-        }
-
         // Inverse Quantization
-        return iQuantTable[m_ArtDE[p][r]];	// Always positive value
+        return iQuantTable[getElement(p, r)];
     }
 
 
@@ -179,7 +121,6 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
 
     @Override
     protected float getDistanceImpl(LocalAbstractObject obj, float distThreshold) {
-
         ObjectRegionShape object = (ObjectRegionShape) obj;
 
         // perform matching
@@ -201,12 +142,12 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
         if (!(obj instanceof ObjectRegionShape))
             return false;
 
-        return Arrays.equals(((ObjectRegionShape)obj).m_ArtDE, m_ArtDE);
+        return Arrays.equals(((ObjectRegionShape)obj).artDE, artDE);
     }
 
     @Override
     public int dataHashCode() {
-        return Arrays.hashCode(m_ArtDE);
+        return Arrays.hashCode(artDE);
     }
 
     @Override
@@ -231,27 +172,19 @@ public class ObjectRegionShape extends LocalAbstractObject implements BinarySeri
      */
     protected ObjectRegionShape(BinaryInput input, BinarySerializator serializator) throws IOException {
         super(input, serializator);
-        for (int i = 0; i < ART_ANGULAR; i++) {
-            for (int j = 0; j < ART_RADIAL; j++) {
-                this.m_ArtDE[i][j] = serializator.readByte(input);
-            }
-        }
+        this.artDE = serializator.readByteArray(input);
     }
 
     @Override
     public int binarySerialize(BinaryOutput output, BinarySerializator serializator) throws IOException {
         int retval = super.binarySerialize(output, serializator);
-        for (int i = 0; i < ART_ANGULAR; i++) {
-            for (int j = 0; j < ART_RADIAL; j++) {
-                retval += serializator.write(output, m_ArtDE[i][j]);
-            }
-        }
+        retval += serializator.write(output, artDE);
         return retval;
     }
 
     @Override
     public int getBinarySize(BinarySerializator serializator) {
-        return  super.getBinarySize(serializator) + (ART_ANGULAR * ART_RADIAL);
+        return super.getBinarySize(serializator) + serializator.getBinarySize(artDE);
     }
     
 }
