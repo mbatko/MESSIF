8fc8ec9f724ea6ae0382b3550faa666b313ed9bd
Michal Batko <batko@fi.muni.cz>
2010-04-27 07:25:30 +0000

    * Moved perforated distance filter to private packages * Fixed bug in object order by unique ID * Fixed bug with when null parameters are passed to AlgorithmStorageBucket, VirtualStorageBucket and DiskStorage * Fixed bulk insert operation's annotation to support single argument correctly * Forced statistic computations for lower/upper bounds on metric function * Added support of passing file names to external extractors * Added support for binary serialization of string objects * Documentation fixes * Cleanup of attributes/constructor/access method

--- messif/utility/http/OperationHandler.java
+++ messif/utility/http/OperationHandler.java
@@ -26,15 +26,37 @@ import messif.operations.AbstractOperation;
 import messif.operations.RankingQueryOperation;
 
 /**
+ * Special handler for {@link AbstractOperation}s.
+ * For {@link RankingQueryOperation ranking query operations} the list of results
+ * with a distance are returned.
+ * Otherwise the returned value represents just the operation status.
+ *
+ * @param <T> the class of the operation returned by the encapsulated processor
+ * @see OperationProcessor
  *
  * @author Michal Batko, Masaryk University, Brno, Czech Republic, batko@fi.muni.cz
  * @author Vlastislav Dohnal, Masaryk University, Brno, Czech Republic, dohnal@fi.muni.cz
  * @author David Novak, Masaryk University, Brno, Czech Republic, david.novak@fi.muni.cz
  */
 public class OperationHandler<T extends AbstractOperation> extends SimpleHandler<T> implements HttpHandler {
+    /** Format of a single object the result using XML */
     private static final String RESULT_XML_TAG = "<result distance=\"%f\" locator=\"%s\"/>";
+    /** Format of a single object the result using JSON */
     private static final String RESULT_JSON_TAG = "[%f,\"%s\"]";
 
+    /**
+     * Creates a new instance of OperationHandler.
+     * @param log the logger to use for logging errors (if <tt>null</tt> is passed, no logging is done)
+     * @param algorithm the algorithm on which the operation is executed
+     * @param operationClass the class of the operation to execute
+     * @param args arguments for the operation's constructor
+     * @param offset index of the first valid argument in the {@code args} array
+     * @param length number of valid arguments in the {@code args} array
+     * @param namedInstances collection of named instances that are used when converting string parameters
+     * @throws IndexOutOfBoundsException if the {@code offset} or {@code length} are not valid for {@code args} array
+     * @throws IllegalArgumentException if the operation does not have an annotated constructor with {@code length} argument or
+     *              if any of the provided {@code args} cannot be converted to the type specified in the operation's constructor
+     */
     public OperationHandler(Logger log, Algorithm algorithm, Class<? extends T> operationClass, String args[], int offset, int length, Map<String, Object> namedInstances) throws IndexOutOfBoundsException, IllegalArgumentException {
         super(log, false, new OperationProcessor<T>(algorithm, operationClass, args, offset, length, namedInstances));
     }
