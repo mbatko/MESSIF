186ca68bcac9e1fd0175860d523a1a45fc301d74
Michal Batko <batko@fi.muni.cz>
2011-03-22 12:24:24 +0000

    * Modified implementation of metaobject map to use the new methods for writing/reading text streams

--- messif/objects/impl/MetaObjectMap.java
+++ messif/objects/impl/MetaObjectMap.java
@@ -17,22 +17,16 @@
 package messif.objects.impl;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.logging.Logger;
-import messif.objects.keys.AbstractObjectKey;
 import messif.objects.LocalAbstractObject;
 import messif.objects.MetaObject;
 import messif.objects.nio.BinaryInput;
@@ -55,17 +49,18 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
     /** Class id for serialization. */
     private static final long serialVersionUID = 1L;
 
-    /****************** Attributes ******************/
+    //****************** Attributes ******************//
 
     /** List of encapsulated objects */
     protected Map<String, LocalAbstractObject> objects;
 
 
-    /****************** Constructors ******************/
+    //****************** Constructors ******************//
     
     /**
      * Creates a new instance of MetaObjectMap from a collection of named objects.
-     * The locatorURI of every object from the collection is set to the provided one.
+     * The locatorURI of every object from the collection is set to the provided
+     * one only if {@code cloneObjects} is requested.
      *
      * @param locatorURI the locator URI for this object and all the provided objects will be set as well
      * @param objects collection of objects with their symbolic names
@@ -86,7 +81,6 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
 
     /**
      * Creates a new instance of MetaObjectMap from a collection of named objects.
-     * The locatorURI of every object from the collection is set to the provided one.
      *
      * @param locatorURI the locator URI for this object and all the provided objects will be set as well
      * @param objects collection of objects with their symbolic names
@@ -105,8 +99,7 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
      *         EOFException is returned if end of the given stream is reached.
      */
     public MetaObjectMap(BufferedReader stream, Set<String> restrictNames) throws IOException {
-        this.objects = new TreeMap<String, LocalAbstractObject>();
-        readObjects(stream, restrictNames);
+        this.objects = readObjects(stream, restrictNames, readObjectsHeader(stream), new TreeMap<String, LocalAbstractObject>());
     }    
 
     /**
@@ -130,20 +123,6 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
     public MetaObjectMap(BufferedReader stream) throws IOException {
         this(stream, (Set<String>)null);
     }    
-
-
-    //****************** Factory method ******************//
-
-    /**
-     * Creates a meta object from the specified file.
-     * The constructor from {@link BufferedReader} is used.
-     * @param file the file to create the object from
-     * @return a new instance of MetaObjectMap
-     * @throws IOException if there was an error reading from the specified file
-     */
-    public static MetaObjectMap create(File file) throws IOException {
-        return new MetaObjectMap(new BufferedReader(new InputStreamReader(new FileInputStream(file))));
-    }
 
 
     //****************** Clonning ******************//
@@ -188,72 +167,9 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
 
     //****************** Text stream I/O ******************//
 
-    /**
-     * Fills this instance of MetaObject from a text stream.
-     * @param stream the text stream to read the objects from
-     * @param restrictNames if not <tt>null</tt> only the names specified in this collection are added to the objects table
-     * @throws IOException when an error appears during reading from given stream,
-     *         EOFException is returned if end of the given stream is reached.
-     */
-    protected void readObjects(BufferedReader stream, Collection<String> restrictNames) throws IOException {
-        // Keep reading the lines while they are comments, then read the first line of the object
-        String line = readObjectComments(stream);
-
-        // The line should have format "URI;name1;class1;name2;class2;..." and URI can be skipped (including the semicolon)
-        String[] uriNamesClasses = line.split(";");
-
-        // Skip the first name if the number of elements is odd
-        int i = uriNamesClasses.length % 2;
-
-        // If the URI locator is used (and it is not set from the previous - this is the old format)
-        if (i == 1) {
-            if ((this.getObjectKey() == null) && (uriNamesClasses[0].length() > 0))
-                setObjectKey(new AbstractObjectKey(uriNamesClasses[0]));
-        }
-
-        // Read objects and add them to the collection
-        for (i++; i < uriNamesClasses.length; i += 2) {
-            try {
-                LocalAbstractObject object = readObject(stream, uriNamesClasses[i]);
-                if (restrictNames == null || restrictNames.contains(uriNamesClasses[i - 1])) {
-                    object.setObjectKey(this.getObjectKey());
-                    objects.put(uriNamesClasses[i - 1], object);
-                }
-            } catch (IOException e) {
-                // Ignore the exception if the object is not loaded anyway
-                if (restrictNames == null || restrictNames.contains(uriNamesClasses[i - 1]))
-                    Logger.getLogger(getClass().getName()).warning(new StringBuffer("Error creating object ").append(uriNamesClasses[i - 1]).append(" for MetaObject with ID ").append(this.getLocatorURI()).append(":\n ").append(e).toString());
-            }
-        }
-    }
-
-    /**
-     * Store this object to a text stream.
-     * This method should have the opposite deserialization in constructor of a given object class.
-     *
-     * @param stream the stream to store this object to
-     * @throws IOException if there was an error while writing to stream
-     */
     @Override
     protected void writeData(OutputStream stream) throws IOException {
-        // Create first line with semicolon-separated names of classes
-        Iterator<Entry<String, LocalAbstractObject>> iterator = objects.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Entry<String, LocalAbstractObject> entry = iterator.next();
-            if (entry.getValue() != null) {
-                // Write object name and class
-                stream.write(entry.getKey().getBytes());
-                stream.write(';');
-                stream.write(entry.getValue().getClass().getName().getBytes());
-                // Do not append semicolon after the last object in the header
-                if (iterator.hasNext())
-                    stream.write(';');           }
-        }
-        stream.write('\n');
-        
-        // Write a line for every object from the list (skip the comments)
-        for (LocalAbstractObject object : objects.values())
-            object.write(stream, false);
+        writeObjects(stream, writeObjectsHeader(stream, objects));
     }
 
 
@@ -291,7 +207,7 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
 
     /**
      * The actual implementation of the metric function.
-     * The distance is computed as the difference of this and <code>obj</code>'s locator hash-codes.
+     * The distance is a trivial metric on locator URIs of this object and {@code obj}.
      * The array <code>metaDistances</code> is ignored.
      *
      * @param obj the object to compute distance to
@@ -302,7 +218,9 @@ public class MetaObjectMap extends MetaObject implements BinarySerializable {
      */
     @Override
     protected float getDistanceImpl(MetaObject obj, float[] metaDistances, float distThreshold) {
-        return Math.abs(getLocatorURI().hashCode() - obj.getLocatorURI().hashCode());
+        String thisLocator = getLocatorURI();
+        String otherLocator = obj.getLocatorURI();
+        return (thisLocator == null ? otherLocator == null : thisLocator.equals(otherLocator)) ? 0 : 1;
     }
 
 
