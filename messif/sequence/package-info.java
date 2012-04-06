e79238e1346ee4f1dce8cbb70ff169364dcbe318
David Novak <david.novak@fi.muni.cz>
2012-04-06 14:10:42 +0000

    * sequence stuff moved to a separate package  * Approx...Params is not used any more and should not be

--- messif/sequence/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Support for working with generic sequences. This package contains
- * interfaces for slicing sequences into subsequences and transforming
- * them (e.g. by a reduction) into different sequences.
- */
-package messif.sequence;
-
